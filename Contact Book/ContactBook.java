bd5144419798f8dc1c7d5071cc3655edc97b7137
missacodes <issam337b@gmail.com>
2026-09-23 20:22:48 +0100

    contact book mini project, using what i learned from chapter 4

--- Contact Book/ContactBook.java
+++ Contact Book/ContactBook.java
@@ -3,5 +3,43 @@ import java.util.Iterator;
 import java.util.Scanner;
 public class ContactBook
 {
-    private ArrayList<String> files;
+    private ArrayList<String> contacts;
+    public ContactBook() {
+    contacts = new ArrayList<>();
+    }
+    public void addContact(String name)
+    {
+        contacts.add(name);
+    
+    }
+    
+    public void listAllContacts() {
+        for (String card: contacts) {
+        System.out.println(card);
+        }
+    }
+    
+    public boolean containsContact(String name) {
+        
+        for (String card:contacts) {
+            if (card.equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void removeContactNamed(String name) {
+        Iterator<String> it = contacts.iterator();
+        while(it.hasNext()) {
+            if (it.next().equals(name)) {
+                it.remove();
+            }
+        }
+    
+    }
+    
+    public int getNumberOfContacts() {
+        return contacts.size();
+    }
 }
